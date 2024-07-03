a6ddde08c869148dc10b30532bc8cab37b4c5949
indira34 <indirabolotbekkyzy70@gmail.com>
2024-07-03 09:56:12 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Выпльнить (+,*,/,%)
+let a=4
+let b=55
+console.log(a + b)
+
+console.log(a * b)
+
+console.log(a / b)
+
+console.log(a  % b)
+
+//вывести в консоль "hello (ваше имя)"
+
+let name = "Indira"
+console.log( "hello " + name)
+
+//вывести в консоль NaN
+console.log(NaN)
+
+//вывести в консоль 10
+ let c = 5
+let e = "5"
+console.log(c + Number(e))
+
+//сравнить и обеяснить почему такой результат
+let f = 2
+let g = undefined
+console.log(2 < undefined)
+//Сравнение (1) и (2) возвращает false.птому что undefined преобразуется в NaN - это специальное числовое значениекоторое вщзвращает false при любых сравнениях,
